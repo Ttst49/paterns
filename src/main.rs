922dccb760d363081a5deceb7a540ef431835d9d
Ttst <thibautstachnick@gmail.com>
2024-04-15 19:18:26 +0200

    add method about if let learning

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
+fn if_let_learning(){
+    let favorite_color :Option<&str> = None;
+    let we_are_tuesday =false;
+    let age: Result<u64,_> = "34".parse();
+
+    if let Some(color) = favorite_color{
+        println!("Using your favorite color {} as background",color);
+    }else if we_are_tuesday {
+        println!("Tuesday it's green day")
+    }else if let Ok(age) = age{
+        if age>30 {
+            println!("Using purple as background")
+        }else {
+            println!("Using orange as background")
+        }
+    }else {
+        println!("Using blue as background")
+    }
+}
+
 
 fn main() {
-    println!("Hello, world!");
+    if_let_learning();
 }
