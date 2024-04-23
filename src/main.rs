d0c2dc81ba9cb02e39c33f0ce84a6c34a9437c62
Ttst <thibautstachnick@gmail.com>
2024-04-23 17:57:14 +0200

    add method that use match

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,15 @@ fn for_pattern_learning(){
     }
 }
 
+fn matching_literals(){
+    let x = 1;
+    match x {
+        1=>println!("one"),
+        2=>println!("two"),
+        3=>println!("three"),
+        _=>println!("anything else")
+    }
+}
 
 
 fn main() {
